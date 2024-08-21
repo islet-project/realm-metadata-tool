ffaba8ea3c4c0e0b00b43412000f1958e9517bdc
Piotr Sawicki <p.sawicki2@samsung.com>
2024-08-21 08:43:54 +0200

    Apply changes according to the remarks from the review
    Use hex crate to dump hexadecimal content of arrays.
    Add default command line options aligned with README.md.
    
    Signed-off-by: Piotr Sawicki <p.sawicki2@samsung.com>

--- src/cli.rs
+++ src/cli.rs
@@ -13,27 +13,27 @@ pub struct Cli {
 pub enum Commands {
     Create {
         /// Path to the manifest file (.yaml)
-        #[arg(short, long)]
+        #[arg(short, long, default_value = "manifest.yaml")]
         manifest: PathBuf,
 
         /// Path to the EC P384 private key file (.pem)
-        #[arg(short, long)]
+        #[arg(short, long, default_value = "private.pem")]
         key: PathBuf,
 
         /// The resulting realm metadata file
-        #[arg(short, long)]
+        #[arg(short, long, default_value = "metadata.bin")]
         output: PathBuf,
     },
 
     Verify {
         /// Path to the realm metadata file
-        #[arg(short, long)]
+        #[arg(short, long, default_value = "metadata.bin")]
         input: PathBuf,
     },
 
     Dump {
         /// Path to the realm metadata file
-        #[arg(short, long)]
+        #[arg(short, long, default_value = "metadata.bin")]
         input: PathBuf,
 
         /// Dump metadata structure in a hexadecimal format
